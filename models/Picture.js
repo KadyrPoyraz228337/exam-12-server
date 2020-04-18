fe5db98d7de4b8ea91f3e0e856939a4ea29fa28b
KadyrPoyraz228337 <poyraz_2001@mail.ru>
2020-04-18 11:34:45 +0600

    Добавил модель картинки

---
+++ models/Picture.js
@@ -0,0 +1,18 @@
+const mongoose = require('mongoose');
+
+const PictureSchema = mongoose.Schema({
+  user: {
+    type: mongoose.Schema.Types.ObjectId,
+    required: true
+  },
+  title: {
+    type: String,
+    required: true
+  },
+  image: {
+    type: String,
+    required: true
+  }
+});
+
+module.exports = mongoose.model('Picture', PictureSchema);
