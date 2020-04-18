a510fb976888f933d4094498a790d3931218fa74
KadyrPoyraz228337 <poyraz_2001@mail.ru>
2020-04-18 15:57:11 +0600

    Реализовал ref для свойства user модели Picture.js

--- models/Picture.js
+++ models/Picture.js
@@ -3,6 +3,7 @@ const mongoose = require('mongoose');
 const PictureSchema = mongoose.Schema({
   user: {
     type: mongoose.Schema.Types.ObjectId,
+    ref: 'User',
     required: true
   },
   title: {
